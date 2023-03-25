558d0ff12c581e113bc3c3132622c3f138a0468b
khalteck <khalidoyeneye@gmail.com>
2023-03-25 17:20:47 +0100

    Clean up #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import "./styles/output.css";
 import Dashboard from "./pages/Dashboard";
-import { useAppContext } from "./contexts/AppContext";
 
 function App() {
-  const { currentPage } = useAppContext();
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
